7fa343a184dcff67132023bffe238cbb87e79c35
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2022-08-04 01:17:41 +1000

    Credit Score

---
+++ Credit_Score_App/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit_Score_App
+{
+    public class Customer
+    {
+        public int BureauScore { get; }
+        public int MissedPaymentCount { get; }
+        public int CompletedPaymentCount { get; }
+        public int AgeInYears { get; }
+        public Customer(int bureauScore, int missedPaymentCount, int completedPaymentCount, int ageInYears)
+        {
+            BureauScore = bureauScore;
+            MissedPaymentCount = missedPaymentCount;
+            CompletedPaymentCount = completedPaymentCount;
+            AgeInYears = ageInYears;
+        }
+    }
+}
